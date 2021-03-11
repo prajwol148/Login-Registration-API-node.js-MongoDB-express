5131336183070cf44e5ac377d83243ab5ad2bd52
prajwol <prajwol.tiwari@gmail.com>
2021-03-11 23:25:29 +0545

    Login/Register done.

---
+++ flutterauth/config/dbconfig.js
@@ -0,0 +1,4 @@
+module.exports = {
+    secret: 'yoursecret',
+    database: 'mongodb+srv://ssjprajwol:ssjprajwol@medhaviauthentication.mpocc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
