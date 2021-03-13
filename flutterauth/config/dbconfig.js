f83df1281b9d65c9c25192d8ab87eb50a30a9547
prajwol <prajwol.tiwari@gmail.com>
2021-03-13 14:56:14 +0545

    make it better

--- flutterauth/config/dbconfig.js
+++ flutterauth/config/dbconfig.js
@@ -1,4 +1,4 @@
 module.exports = {
     secret: 'yoursecret',
-    database: 'mongodb+srv://ssjprajwol:ssjprajwol@medhaviauthentication.mpocc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    database: 'mongodb://sampanna:sampanna@knocadoor-shard-00-00.vniit.mongodb.net:27017,knocadoor-shard-00-01.vniit.mongodb.net:27017,knocadoor-shard-00-02.vniit.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-8eed0t-shard-0&authSource=admin&retryWrites=true&w=majority'
 }
